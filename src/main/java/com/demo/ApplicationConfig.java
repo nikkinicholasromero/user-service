995e91239f6fc9712c675ca0fde5e21600b9f0b6
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-06-19 13:03:56 -0400

    Code cleanup

--- src/main/java/com/demo/ApplicationConfig.java
+++ src/main/java/com/demo/ApplicationConfig.java
@@ -1,13 +1,10 @@
 package com.demo;
 
-import com.demo.mock.MockRestTemplateBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -15,7 +12,6 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
@@ -81,11 +77,5 @@ public class ApplicationConfig implements WebMvcConfigurer {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(entityManager().getObject());
         return transactionManager;
-    }
-
-    @Bean
-    @Profile("mock")
-    public RestTemplateBuilder restTemplateBuilder() {
-        return new MockRestTemplateBuilder();
     }
 }
--- src/test/java/com/demo/ApplicationTest.java
+++ src/test/java/com/demo/ApplicationTest.java
@@ -1,24 +1,10 @@
 package com.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
     @Test
     public void main() {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class)).isBlank();
+        Application.main(new String[]{});
     }
 }
