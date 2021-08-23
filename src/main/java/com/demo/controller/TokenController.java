565de0216d71f7e8a76f4f6f936285056eb4548f
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-08-23 07:55:53 -0400

    Refactored response objects

--- src/main/java/com/demo/controller/TokenController.java
+++ src/main/java/com/demo/controller/TokenController.java
@@ -1,35 +1,20 @@
 package com.demo.controller;
 
-import com.demo.model.AuthenticationResponse;
 import com.demo.model.TokenDecodeRequest;
 import com.demo.model.TokenRefreshRequest;
 import com.demo.service.TokenDecodeService;
 import com.demo.service.TokenRefreshService;
-import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.function.Function;
-import java.util.function.Supplier;
-
 @Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("/tokens")
 public class TokenController {
-    private static final Function<Claims, ResponseEntity<Claims>> successDecode =
-            response -> new ResponseEntity<>(response, HttpStatus.OK);
-    private static final Supplier<ResponseEntity<Claims>> unauthorizedDecode =
-            () -> new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-
-    private static final Function<AuthenticationResponse, ResponseEntity<AuthenticationResponse>> successRefresh =
-            response -> new ResponseEntity<>(response, HttpStatus.OK);
-    private static final Supplier<ResponseEntity<AuthenticationResponse>> unauthorizedRefresh =
-            () -> new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-
     @Autowired
     private TokenDecodeService tokenDecodeService;
 
@@ -38,11 +23,15 @@ public class TokenController {
 
     @PostMapping("/decode")
     public ResponseEntity<?> decode(@RequestBody TokenDecodeRequest request) {
-        return tokenDecodeService.decode(request.getToken()).map(successDecode).orElseGet(unauthorizedDecode);
+        return tokenDecodeService.decode(request.getToken())
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<?> refresh(@RequestBody TokenRefreshRequest request) {
-        return tokenRefreshService.refresh(request.getRefreshToken()).map(successRefresh).orElseGet(unauthorizedRefresh);
+        return tokenRefreshService.refresh(request.getRefreshToken())
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
     }
 }
--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -9,24 +9,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.function.Function;
-import java.util.function.Supplier;
-
 @Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private static final Function<AuthenticationResponse, ResponseEntity<AuthenticationResponse>> success =
-            response -> new ResponseEntity<>(response, HttpStatus.OK);
-    private static final Supplier<ResponseEntity<AuthenticationResponse>> unauthorized =
-            () -> new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-
     @Autowired
     private AuthenticationOrchestrator orchestrator;
 
     @PostMapping("/auth")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        return orchestrator.orchestrate(request).map(success).orElseGet(unauthorized);
+        return orchestrator.orchestrate(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
     }
 }
